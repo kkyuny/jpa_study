35206d4fbce7cee2ab63e8be4ba9830289fd2cbf
kyun <kyun9911@gmail.com>
2024-10-21 22:19:47 +0900

    Ch 04. first commit

---
+++ src/test/java/com/study/jpa/bookmanager/repository/UsersRepositoryTest4.java
@@ -0,0 +1,17 @@
+package com.study.jpa.bookmanager.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+@SpringBootTest
+public class UsersRepositoryTest4 {
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Test
+    void test() {
+    }
+}
